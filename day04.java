ac0d65ca3cd21be6e890391f574c8fd498b70b5d
afthabvp <afthab01@gmail.com>
2016-01-06 01:18:51 +0530

    day05

---
+++ day04.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    void out(int a,int b,int N){
+        int sum = a;
+        for(int j=0;j<N;j++){
+            sum +=(int)Math.pow(2, j)*b;
+            System.out.print(sum+" ");
+            
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        
+       try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n = Integer.parseInt(br.readLine());
+      	    for (int i = 0; i < n; i++) {
+              String [] input = br.readLine().split(" ");
+              sol.out(Integer.parseInt(input[0]),Integer.parseInt(input[1]),Integer.parseInt(input[2]));
+             }
+           }catch(IOException io){
+            io.printStackTrace();
+          }	
+        
+}
+}
