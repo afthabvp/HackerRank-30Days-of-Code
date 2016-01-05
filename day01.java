56d72fabf5fc51392ccd99230e8b489305edef33
afthabvp <afthab01@gmail.com>
2016-01-05 23:58:21 +0530

    first commit

---
+++ day01.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println( "Primitive : double");  // 5.35
+        System.out.println( "Primitive : char");    // 'a'
+        System.out.println( "Primitive : boolean"); // false
+        System.out.println( "Primitive : int");     // 100
+        System.out.println( "Referenced : String");  // "I am a code monkey"
+        System.out.println( "Primitive : boolean");  // true
+        System.out.println( "Primitive : double");  // 17.3
+        System.out.println( "Primitive : char");  // 'c'
+        System.out.println( "Referenced : String");  // "derp"    
+    }
+}
