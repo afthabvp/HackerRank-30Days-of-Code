a5cdb107b0e9981c8b970bf8f06a848cd40a674c
afthabvp <afthab01@gmail.com>
2016-01-17 19:48:10 +0530

    day6 to day 9

---
+++ day09.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		//Take Input
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		sc.close();
+
+
+		int gcd=find_gcd(a,b);
+		System.out.println(gcd);
+	}
+	static  int find_gcd(int a,int b){
+		//Write the base condition
+		if (a == b)
+			return a;
+		else
+			return find_gcd(Math.max(a,b) - Math.min(a,b),Math.min(a,b));
+	}
+}
+
